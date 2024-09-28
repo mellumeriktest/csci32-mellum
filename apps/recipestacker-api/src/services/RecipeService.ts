8ca8cd2ecfd5764029df4ca5590d41a5a5763922
mellumeriktest <mellum.erik+test@gmail.com>
2024-09-27 22:10:48 -0700

    Complete POST for create recipe endpoint

--- apps/recipestacker-api/src/services/RecipeService.ts
+++ apps/recipestacker-api/src/services/RecipeService.ts
@@ -18,11 +18,21 @@ interface FindOneRecipeProps {}
 
 interface FindManyRecipeProps {}
 
-interface CreateIngredientMeasurementProps {}
+interface CreateIngredientMeasurementProps {
+  ingredient_id?: string
+  ingredient_name?: string
+  ingredient_description?: string
+  unit: string
+  quantity: number
+}
 
 interface UpdateOneRecipeProps {}
 
-interface CreateOneRecipeProps {}
+interface CreateOneRecipeProps {
+  name: string
+  description: string
+  ingredient_measurements: CreateIngredientMeasurementProps[]
+}
 
 interface GetRecipeOrderByProps {}
 
@@ -43,5 +53,42 @@ export class RecipeService {
 
   async findManyRecipes(props: FindManyRecipeProps) {}
 
-  async createOneRecipe(props: CreateOneRecipeProps) {}
+  async createOneRecipe(props: CreateOneRecipeProps) {
+    const { name, description, ingredient_measurements } = props
+    const spoof_user_id = 'cm1lod0xh0000n9ayf9n5g7zo'
+    const prismaData = {
+      data: {
+        user: {
+          connect: {
+            user_id: spoof_user_id,
+          },
+        },
+        name,
+        description,
+        ingredient_measurements: {
+          create: ingredient_measurements.map(
+            ({ ingredient_id, ingredient_name, ingredient_description, unit, quantity }) => ({
+              ingredient: ingredient_id
+                ? {
+                    connect: {
+                      ingredient_id,
+                    },
+                  }
+                : {
+                    create: {
+                      name: ingredient_name,
+                      description: ingredient_description,
+                    },
+                  },
+              quantity,
+              unit,
+            }),
+          ),
+        },
+      },
+    }
+    this.logger.info('Check out this body', prismaData)
+    const recipe = await this.prisma.recipe.create(prismaData)
+    return recipe
+  }
 }
